a59969200b2ab2a0c17c4b31186cb43c5867b330
William-CarterG <wgcarter@miuandes.cl>
2022-09-08 17:39:31 -0400

    feat(HomeTasksFragment created): Created the HomeTasksFragment, where you can see all tasks for a user. The view developed is for a developer type of user, but the example will be from the perspective of a team lead.

---
+++ app/src/main/java/cl/uandes/taskapp/ui/HomeTasksFragment.kt
@@ -0,0 +1,30 @@
+package cl.uandes.taskapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import android.util.Patterns
+import androidx.navigation.fragment.findNavController
+import cl.uandes.taskapp.databinding.FragmentHomeTasksBinding
+
+class HomeTasksFragment : Fragment() {
+    private lateinit var binding: FragmentHomeTasksBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentHomeTasksBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    //TODO: Navigate to HomeProjects and to TaskExample
+}
