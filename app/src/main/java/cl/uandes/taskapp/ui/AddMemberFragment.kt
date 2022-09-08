33d1c9a649530f79403f250605f8c5c24f6e8ee2
William-CarterG <wgcarter@miuandes.cl>
2022-09-08 14:42:08 -0400

    feat(Add member to project view created): Created the AddMemberFragment, for adding new members to a certain project (without the actual implementation of it, of course).

---
+++ app/src/main/java/cl/uandes/taskapp/ui/AddMemberFragment.kt
@@ -0,0 +1,35 @@
+package cl.uandes.taskapp.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import cl.uandes.taskapp.databinding.FragmentAddMemberBinding
+
+class AddMemberFragment : Fragment() {
+    private lateinit var binding: FragmentAddMemberBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentAddMemberBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addMemberToProject()
+    }
+
+    private fun addMemberToProject(){
+        val addMemberButton = binding.addExampleButton
+        addMemberButton.setOnClickListener {
+            //TODO: Logic of adding a new member to project
+            //...
+        }
+    }
+}
