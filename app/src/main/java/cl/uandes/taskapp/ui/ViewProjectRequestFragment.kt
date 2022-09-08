55e5d66d072bddb929429a635150557edd5d77fc
William-CarterG <wgcarter@miuandes.cl>
2022-09-08 16:21:38 -0400

    feat(Navigation to ProjectProfile from ViewPendingRequest): Implemented navigation from the ViewPendingRequestFragment to the ProjectProfileFragment.

--- app/src/main/java/cl/uandes/taskapp/ui/ViewProjectRequestFragment.kt
+++ app/src/main/java/cl/uandes/taskapp/ui/ViewProjectRequestFragment.kt
@@ -37,7 +37,7 @@ class ViewProjectRequestFragment : Fragment() {
     private fun returnToHomeProjects(){
         val leaveAddMembersButton = binding.leaveAddMembersButton
         leaveAddMembersButton.setOnClickListener {
-            val action = AddMemberFragmentDirections.actionAddMemberFragmentToProjectProfileFragment()
+            val action = ViewProjectRequestFragmentDirections.actionViewProjectRequestFragmentToProjectProfileFragment()
             findNavController().navigate(action)
         }
     }
