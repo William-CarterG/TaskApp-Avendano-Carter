ce7b8e75b4f7b6af817cac8222483bda36d5e7b3
carmencitaap <cmavendano@miuandes.cl>
2022-09-29 18:58:10 -0300

    feat(task): Added Task class and some instances

--- app/src/main/java/cl/uandes/taskapp/data/datasources/InMemoryDataSource.kt
+++ app/src/main/java/cl/uandes/taskapp/data/datasources/InMemoryDataSource.kt
@@ -1,8 +1,11 @@
 package cl.uandes.taskapp.data.datasources
 
+import cl.uandes.taskapp.data.model.Task
 import cl.uandes.taskapp.data.model.User
 
 object InMemoryDataSource {
     val users = mutableListOf( User(1,"hola@chao.cl", "123456", "teamlead"),
                                User(2,"chao@hola.com","123456","user"))
+    val tasks = mutableListOf( Task(1, "Add tasks menu", "Proyecto 1. Assigned by Julio Cortés"),
+                               Task(2, "Implement navigation", "Blog App. Assigned by María Pía Sánchez"))
 }
---
+++ app/src/main/java/cl/uandes/taskapp/data/model/Task.kt
@@ -0,0 +1,7 @@
+package cl.uandes.taskapp.data.model
+
+data class Task (
+    var id: Long?,
+    val title: String,
+    val description: String
+        )
