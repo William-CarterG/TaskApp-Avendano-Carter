b32d3e68b5e47ce8e8295c2a2f8a59d779074f58
carmencitaap <cmavendano@miuandes.cl>
2022-09-29 20:50:16 -0300

    feat(task-model): Added attributes and instances to task model

--- app/src/main/java/cl/uandes/taskapp/data/datasources/InMemoryDataSource.kt
+++ app/src/main/java/cl/uandes/taskapp/data/datasources/InMemoryDataSource.kt
@@ -6,6 +6,6 @@ import cl.uandes.taskapp.data.model.User
 object InMemoryDataSource {
     val users = mutableListOf( User(1,"hola@chao.cl", "123456", "teamlead"),
                                User(2,"chao@hola.com","123456","user"))
-    val tasks = mutableListOf( Task(1, "Add tasks menu", "Proyecto 1. Assigned by Julio Cortés"),
-                               Task(2, "Implement navigation", "Blog App. Assigned by María Pía Sánchez"))
+    val tasks = mutableListOf( Task(1, "Add tasks menu", "Proyecto 1", "Julio Cortés","You have to add the tasks menu to the fragment.","08/09/2022" ),
+                               Task(2, "Implement navigation", "Blog App", "María Pía Sánchez","Add navigation's actions.","13/09/2022"))
 }
