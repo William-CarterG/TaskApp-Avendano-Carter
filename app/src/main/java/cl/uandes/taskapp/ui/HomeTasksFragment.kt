70dacd4789c238311e8f24657f829f224ae52bad
carmencitaap <cmavendano@miuandes.cl>
2022-09-30 17:31:09 -0300

    feat(home-tasks): Implemented the beggining of the recyclerview. Added the taskItemAdapter and its items

--- app/src/main/java/cl/uandes/taskapp/ui/HomeTasksFragment.kt
+++ app/src/main/java/cl/uandes/taskapp/ui/HomeTasksFragment.kt
@@ -5,19 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import android.util.Patterns
 import androidx.navigation.fragment.findNavController
 import cl.uandes.taskapp.databinding.FragmentHomeTasksBinding
 import cl.uandes.taskapp.R
+import androidx.core.os.bundleOf
+import androidx.recyclerview.widget.LinearLayoutManager
+import cl.uandes.taskapp.data.model.Task
+import cl.uandes.taskapp.data.datasources.InMemoryDataSource
 
-class HomeTasksFragment : Fragment() {
+class HomeTasksFragment : Fragment(), TaskItemAdapter.ActionListener {
     private lateinit var binding: FragmentHomeTasksBinding
+    private lateinit var taskItemAdapter: TaskItemAdapter
+    private var allTasks = InMemoryDataSource.tasks
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View {
+        taskItemAdapter = TaskItemAdapter(allTasks,this)
         binding = FragmentHomeTasksBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
         return binding.root
@@ -27,8 +33,10 @@ class HomeTasksFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val homeTasksListView = binding.recyclerViewHomeTasks
+        homeTasksListView.layoutManager = LinearLayoutManager(context)
+        homeTasksListView.adapter = taskItemAdapter
+
         toHomeProjects()
-        toTaskProfileExample()
         toCreateTask()
         toUserProfile()
     }
@@ -37,14 +45,6 @@ class HomeTasksFragment : Fragment() {
         val homeProjectsButton = binding.homeProjectsButton
         homeProjectsButton.setOnClickListener {
             val action = HomeTasksFragmentDirections.actionHomeTasksFragmentToHomeProjectsFragment()
-            findNavController().navigate(action)
-        }
-    }
-
-    private fun toTaskProfileExample(){
-        val taskProfileExampleButton = binding.taskProfileExampleButton
-        taskProfileExampleButton.setOnClickListener {
-            val action = HomeTasksFragmentDirections.actionHomeTasksFragmentToTaskProfileFragment()
             findNavController().navigate(action)
         }
     }
@@ -61,5 +61,10 @@ class HomeTasksFragment : Fragment() {
         profileButton.setOnClickListener {
             findNavController().navigate(R.id.action_homeTasksFragment_to_userProfileFragment)
         }
+    }
+
+    override fun goToTaskDetails(task: Task) {
+        val bundle = bundleOf("taskTitle" to task.title)
+        findNavController().navigate(R.id.action_homeTasksFragment_to_taskProfileFragment, bundle)
     }
 }
---
+++ app/src/main/java/cl/uandes/taskapp/ui/TaskItemAdapter.kt
@@ -0,0 +1,51 @@
+package cl.uandes.taskapp.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import cl.uandes.taskapp.R
+import cl.uandes.taskapp.data.model.Task
+
+class TaskItemAdapter (
+    private val tasks: MutableList<Task>,
+    private val actionListener: ActionListener
+) : RecyclerView.Adapter<TaskItemAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val taskItem = itemView.findViewById<ViewGroup>(R.id.taskItem)
+        val taskTitle = itemView.findViewById<TextView>(R.id.taskTitleTextView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val taskItemView: View = inflater.inflate(R.layout.home_task_item, parent, false)
+
+        return ViewHolder(taskItemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val task: Task = tasks[position]
+
+        val detailsButton = holder.taskItem
+
+        val title = holder.taskTitle
+
+        title.text = task.title
+
+
+        detailsButton.setOnClickListener {
+            actionListener.goToTaskDetails(task)
+        }
+    }
+    override fun getItemCount(): Int {
+        return tasks.size
+    }
+
+    interface ActionListener {
+        fun goToTaskDetails(task: Task)
+    }
+}
