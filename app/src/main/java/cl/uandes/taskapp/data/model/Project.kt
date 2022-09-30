d17ad011c78eb2e7abc616871a218ceca0d660b0
carmencitaap <cmavendano@miuandes.cl>
2022-09-29 21:53:40 -0300

    feat(project): Created Project model and added instances of it to inMemoryDataSource

---
+++ app/src/main/java/cl/uandes/taskapp/data/model/Project.kt
@@ -0,0 +1,13 @@
+package cl.uandes.taskapp.data.model
+
+data class Project (
+    var id: Long?,
+    val title: String,
+    val description: String,
+    val admin: String,
+    val participant: String,
+    val creationDate: String,
+    val deadline: String,
+    val percentageCompletion: String,
+    val status: String
+        )
