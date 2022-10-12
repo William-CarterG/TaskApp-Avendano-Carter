4cca8ec8b9e97d68e69908f597974efa9c892559
William-CarterG <wgcarter@miuandes.cl>
2022-10-12 19:55:38 -0300

    feat(created database): Created database to implement repository pattern, with all its entities.

---
+++ app/src/main/java/cl/uandes/taskapp/data/db/AppDatabase.kt
@@ -0,0 +1,39 @@
+package cl.uandes.taskapp.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import cl.uandes.taskapp.data.db.dao.ProjectDao
+import cl.uandes.taskapp.data.db.entity.Project
+import cl.uandes.taskapp.data.db.entity.Task
+import cl.uandes.taskapp.data.db.entity.User
+
+
+@Database(
+    entities = [Project::class, Task::class, User::class],
+    version = 1,
+    exportSchema = false
+)
+
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun getNoteDao(): ProjectDao
+
+    companion object {
+        private const val DB_NAME = "taskapp_database.db"
+        @Volatile private var instance: AppDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            AppDatabase::class.java,
+            DB_NAME
+        ).build()
+    }
+}
