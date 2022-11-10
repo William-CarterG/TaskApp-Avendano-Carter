6d6f007fc45599f9bd684523752f75c04de70b0a
carmencitaap <cmavendano@miuandes.cl>
2022-11-10 18:04:58 -0300

    feat(view-model): Initialized DB and repository in the CreateProjectViewModel

--- app/src/main/java/cl/uandes/taskapp/ui/CreateProject/CreateProjectViewModel.kt
+++ app/src/main/java/cl/uandes/taskapp/ui/CreateProject/CreateProjectViewModel.kt
@@ -1,17 +1,28 @@
 package cl.uandes.taskapp.ui.CreateProject
 
+import android.app.Application
 import androidx.lifecycle.*
 import cl.uandes.taskapp.data.datasources.InMemoryDataSource
+import cl.uandes.taskapp.data.db.AppDatabase
 import cl.uandes.taskapp.data.db.entity.Project
 import cl.uandes.taskapp.data.repository.ProjectRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class CreateProjectViewModel (private val repository: ProjectRepository) : ViewModel(){
-    val allProjects: LiveData<List<Project>> = repository.allProjects.asLiveData()
-    /**
-     * Launching a new coroutine to insert the data in a non-blocking way
-     */
-    fun insert(project: Project) = viewModelScope.launch {
-        repository.insertProject(project)
+class CreateProjectViewModel(application: Application) : AndroidViewModel(application) {
+    private val repository: ProjectRepository
+    private val allProjects: LiveData<List<Project>>
+
+    init {
+        val db = AppDatabase.invoke(application)
+        val projectDao = db.getProjectDao()
+        repository = ProjectRepository(db,projectDao)
+        allProjects = repository.allProjects.asLiveData()
+    }
+
+    fun insert(project: Project) {
+        viewModelScope.launch(Dispatchers.IO){
+            repository.insertProject(project)
+        }
     }
 }
